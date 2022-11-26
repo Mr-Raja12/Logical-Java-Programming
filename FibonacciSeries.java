683c5a442d3e42ecf152d87611e17993ceb83aca
Mr-Raja12 <rk9576899082@gmail.com>
2022-11-26 23:51:12 +0530

    Added a program to print Fabonacci Series

--- FibonacciSeries.java
+++ FibonacciSeries.java
@@ -1,5 +1,17 @@
 package com.bl.logicalprogram;
-
 public class FibonacciSeries {
-
+	 public static void main(String[] args) {
+	        int number1=0,number2=1,number3,i,count=15;
+	        System.out.print(number1+" "+number2); 
+	        /**
+	         * loop starts from 2 because 0 and 1 are already printed
+	         */
+	        for(i=2;i<count;++i)
+	        {
+	        	number3=number1+number2;
+	            System.out.print(" "+number3);
+	            number1=number2;
+	            number2=number3;
+	        }
+      }
 }
