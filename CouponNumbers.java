0fa3b35b22e6db00bbf2e09513235d582fde8825
Mr-Raja12 <rk9576899082@gmail.com>
2022-11-27 19:42:08 +0530

    Added a program to print total random number needed to have all distinct numbers.

---
+++ CouponNumbers.java
@@ -0,0 +1,31 @@
+package com.bl.logicalprogram;
+import java.util.Random;
+public class CouponNumbers {
+	/*
+	*Purpose: Given N distinct Coupon Numbers, how many random numbers do you need to generate that distinct coupon number?
+	**/
+	public static void main(String[] args) {
+        int winnerCouponNo = 123456;
+        int couponNumber = 0;
+        int couponCount = 0;
+
+        while (couponNumber != winnerCouponNo){
+            couponNumber = generateCoupon();
+            couponCount++;
+            System.out.println(couponNumber);
+        }
+        System.out.println("winner coupon : " + couponNumber);
+        System.out.println("No of coupons to match winner:" + couponCount);
+    }
+
+    static int generateCoupon(){
+        int num = 0;
+        Random random = new Random();
+        while (num < 100000){
+            num = random.nextInt(999999);
+        }
+        return num;
+    }
+}
+
+
